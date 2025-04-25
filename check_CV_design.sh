9fc88a78b693b1640d7465f9fbd595a39c9b896c
Morillon <54363648+MorillonLab@users.noreply.github.com>
2025-04-25 18:30:35 +0200

    Update check_CV_design.sh

--- check_CV_design.sh
+++ check_CV_design.sh
@@ -1,47 +1,12 @@
 #!/bin/bash
 
+######## input variables ########
 
-#input_dir="/media/marcgabriel/saylar9/Nouritza_DESeq2_batch123_without_10BadSamples_batchesInfoIncluded_22_02_2023/Hugues_cross_validation_method_analysis_RCB0_vs_RCB123_curie_and_canada/"
-#input_dir="/media/marcgabriel/saylar9/Nouritza_DESeq2_batch123_without_10BadSamples_batchesInfoIncluded_22_02_2023/Hugues_cross_validation_method_analysis_RCB0_vs_RCB123/"
-#input_dir="/media/marcgabriel/saylar18/results_10_F_marc/design/"
-#input_dir="/media/marcgabriel/saylar9/Nouritza_DESeq2_batch123_without_10BadSamples_batchesInfoIncluded_22_02_2023/Hugues_cross_validation_method_analysis_RCB0_vs_RCB123_curie_and_canada_10folds5rep/"
-#input_dir="/media/marcgabriel/saylar6/Nouritza_kmerCounts_Curie_and_canada_design_randomizedAndSplitInTrainAndTest/cross_validation_method_analysis_RCB0_vs_RCB123_curie_and_canada_5folds10rep_onTrain/"
-#input_dir="/media/marcgabriel/saylar6/Nouritza_kmerCounts_Curie_and_canada_design_randomizedAndSplitInTrainAndTest_wanted_train_60/CV_design_dir/"
-#input_dir="/media/marcgabriel/saylar14/Anna_Hope_kmers/CV_design_dir"
-#input_dir="/media/marcgabriel/saylar6/Nouritza_genecounts_01_05_2024_Curie_and_canada_design_randomizedAndSplitInTrainAndTest_wanted_train_60_genes_test7/CV_design_dir/"
+input_dir="/media/marcgabriel/saylar6/Nouritza_genecounts_01_05_2024_Curie_and_canada_design_randomizedAndSplitInTrainAndTest_wanted_train_60_genes_test7/CV_design_dir/"
 
-#input_dir="/media/marcgabriel/saylar14/Anna_Hope_kmers/CV_design_hyperplasia_vs_tumor_dir/"
+output_dir="/media/marcgabriel/saylar6/Nouritza_genecounts_01_05_2024_Curie_and_canada_design_randomizedAndSplitInTrainAndTest_wanted_train_60_genes_test7/CV_design_dir/"
 
-input_dir="/media/marcgabriel/saylar14/Anna_Hope_kmers/CV_design_63samples_BPH_or_healthy_vs_tumor_dir/"
-
-#output_dir="/media/marcgabriel/saylar9/Nouritza_DESeq2_batch123_without_10BadSamples_batchesInfoIncluded_22_02_2023/Hugues_cross_validation_method_analysis_RCB0_vs_RCB123_curie_and_canada/"
-#output_dir="/media/marcgabriel/saylar9/Nouritza_DESeq2_batch123_without_10BadSamples_batchesInfoIncluded_22_02_2023/Hugues_cross_validation_method_analysis_RCB0_vs_RCB123/"
-#output_dir="/media/marcgabriel/saylar18/results_10_F_marc/design/"
-#output_dir="/media/marcgabriel/saylar9/Nouritza_DESeq2_batch123_without_10BadSamples_batchesInfoIncluded_22_02_2023/Hugues_cross_validation_method_analysis_RCB0_vs_RCB123_curie_and_canada_10folds5rep/"
-#output_dir="/media/marcgabriel/saylar6/Nouritza_kmerCounts_Curie_and_canada_design_randomizedAndSplitInTrainAndTest/cross_validation_method_analysis_RCB0_vs_RCB123_curie_and_canada_5folds10rep_onTrain/"
-#output_dir="/media/marcgabriel/saylar6/Nouritza_kmerCounts_Curie_and_canada_design_randomizedAndSplitInTrainAndTest_wanted_train_60/CV_design_dir/"
-#output_dir="/media/marcgabriel/saylar14/Anna_Hope_kmers/CV_design_dir/"
-
-#output_dir="/media/marcgabriel/saylar6/Nouritza_genecounts_01_05_2024_Curie_and_canada_design_randomizedAndSplitInTrainAndTest_wanted_train_60_genes_test7/CV_design_dir/"
-
-#output_dir="/media/marcgabriel/saylar14/Anna_Hope_kmers/CV_design_hyperplasia_vs_tumor_dir/"
-
-output_dir="/media/marcgabriel/saylar14/Anna_Hope_kmers/CV_design_63samples_BPH_or_healthy_vs_tumor_dir/"
-
-
-#design="/media/marcgabriel/saylar9/Nouritza_DESeq2_batch123_without_10BadSamples_batchesInfoIncluded_22_02_2023/DESeq_norm_curie_canada_RCB0_vs_RCBothers/Design_for_CV_curie_canada_RCB0_vs_RCBothers.tsv"
-#design="/media/marcgabriel/saylar9/Nouritza_DESeq2_batch123_without_10BadSamples_batchesInfoIncluded_22_02_2023/design_for_CV_RCB0_vs_RCB123.tsv"
-#design="/media/marcgabriel/saylar9/Nouritza_DESeq2_batch123_without_10BadSamples_batchesInfoIncluded_22_02_2023/design_for_CV_RCB0_vs_RCB123.tsv"
-#design="/media/marcgabriel/saylar9/Nouritza_DESeq2_batch123_without_10BadSamples_batchesInfoIncluded_22_02_2023/DESeq_norm_curie_canada_RCB0_vs_RCBothers/Design_for_CV_curie_canada_RCB0_vs_RCBothers.tsv"
-#design="/media/marcgabriel/saylar6/Nouritza_kmerCounts_Curie_and_canada_design_randomizedAndSplitInTrainAndTest/train_design.tsv"
-#design="/media/marcgabriel/saylar6/Nouritza_kmerCounts_Curie_and_canada_design_randomizedAndSplitInTrainAndTest_wanted_train_60/train_design.tsv"
-#design="/media/marcgabriel/saylar14/Anna_Hope_kmers/design_for_cross_validation_spliting.tsv"
-
-#design="/media/marcgabriel/saylar6/Nouritza_genecounts_01_05_2024_Curie_and_canada_design_randomizedAndSplitInTrainAndTest_wanted_train_60_genes_test7/train_design.tsv"
-
-#design="/media/marcgabriel/saylar10/jellyfish_index_gencode32_hyperplasia_vs_tumor/design_for_cross_validation_spliting_hyperplasia_vs_tumor.tsv"
-
-design="/media/marcgabriel/saylar14/Anna_Hope_kmers/BPH_or_healthy_vs_tumor_design_63samples.tsv"
+design="/media/marcgabriel/saylar6/Nouritza_genecounts_01_05_2024_Curie_and_canada_design_randomizedAndSplitInTrainAndTest_wanted_train_60_genes_test7/train_design.tsv"
 
 getTransposedLines="/home/marcgabriel/Desktop/scripts/getTransposedLines.awk"
 
@@ -55,9 +20,9 @@ repeat_num=5
 
 series=($(seq 0 $(($repeat_num-1)) ))
 
-#exp_name="10folds_5timesRepeated_CV"
-
 exp_name="5folds_5timesRepeated_CV"
+
+#########################################
 
 
 all_transposed_matrix=()
